881233ff59eb94870e70cd52aa32d113d08f9268
sivasankar519 <sivasankar519@gmail.com>
2015-10-03 13:23:39 -0400

    Initial Commit

---
+++ Flickr_Recent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flickr_Recent
+//
+//  Created by SIVASANKAR DEVABATHINI on 10/3/15.
+//  Copyright (c) 2015 SIVASANKAR DEVABATHINI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Flickr_RecentTests/Flickr_RecentTests.m
@@ -0,0 +1,40 @@
+//
+//  Flickr_RecentTests.m
+//  Flickr_RecentTests
+//
+//  Created by SIVASANKAR DEVABATHINI on 10/3/15.
+//  Copyright (c) 2015 SIVASANKAR DEVABATHINI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Flickr_RecentTests : XCTestCase
+
+@end
+
+@implementation Flickr_RecentTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
