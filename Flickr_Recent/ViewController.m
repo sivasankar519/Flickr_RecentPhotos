b60c1cd8dab12c4701cfa93baa700033f040b505
sivasankar519 <sivasankar519@gmail.com>
2015-10-03 19:44:55 -0400

    Intial

--- Flickr_Recent/ViewController.m
+++ Flickr_Recent/ViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "ViewController.h"
+#import "MBProgressHUD.h";
 
-@interface ViewController ()
+static NSString * const API_KEY = @"d515b0f9f2c88d498153db3c68649bbe";
+
+static NSString * const recentPhotosURL = @"https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=%@&format=json&nojsoncallback=1";
+
+@interface ViewController (){
+    NSArray *recentPhotos;
+    NSMutableArray *urlsArray;
+    int photoIndex;
+    MBProgressHUD *HUD;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+@property (weak, nonatomic) IBOutlet UILabel *picTitle;
 
 @end
 
@@ -16,12 +28,80 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self getRecentPhotos];
+    HUD = [[MBProgressHUD alloc]init];
+    [self.view addSubview:HUD];
+    HUD.mode = MBProgressHUDModeIndeterminate;
+    HUD.labelText = @"Loading";
+    [HUD removeFromSuperViewOnHide];
+    [HUD show:YES];
+    
+    UISwipeGestureRecognizer * swipeleft=[[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeleft:)];
+    swipeleft.direction=UISwipeGestureRecognizerDirectionLeft;
+    [self.view addGestureRecognizer:swipeleft];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)getRecentPhotos{
+    
+    photoIndex = 0;
+    
+    NSString *urlString = [NSString stringWithFormat:recentPhotosURL, API_KEY];
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:[NSURL URLWithString:urlString]
+            completionHandler:^(NSData *data,
+                                NSURLResponse *response,
+                                NSError *error) {
+                recentPhotos =  [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil][@"photos"][@"photo"];
+                
+                urlsArray = [[NSMutableArray alloc]init];
+                for (int index = 0; index < recentPhotos.count ; index++){
+                    [urlsArray addObject:[self generatePhotoURL:recentPhotos[index]]];
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self.imgView setImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:urlsArray[photoIndex]]]];
+                    self.picTitle.text = recentPhotos[photoIndex][@"title"];
+                    HUD.hidden = YES;
+                });
+            }]
+     resume];
+}
+
+-(NSURL*)generatePhotoURL:(NSDictionary*)photoDict{
+    
+    //https://farm{farm-id}.static.flickr.com/{server-id}/{id}_{secret}.jpg
+    
+    static NSString *baseStr = @"static.flickr.com/";
+    
+    NSMutableString *URLString = [NSMutableString stringWithString:@"https://"];
+    [URLString appendFormat:@"farm%@.",photoDict[@"farm"]];
+    
+
+    [URLString appendString:baseStr];
+    [URLString appendFormat:@"%@/%@_%@.jpg",photoDict[@"server"],photoDict[@"id"],photoDict[@"secret"]];
+    
+    return [NSURL URLWithString:URLString];
+    
+    
+}
+
+-(void)swipeleft:(UIGestureRecognizer*)gesture{
+    
+    if(photoIndex >= urlsArray.count) return;
+    
+    photoIndex++;
+    [self.imgView setImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:urlsArray[photoIndex]]]];
+    self.picTitle.text = recentPhotos[photoIndex][@"title"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
