5a5d5f203d6f4095f64ca42ade840c146681f516
sivasankar519 <sivasankar519@gmail.com>
2015-10-07 16:38:37 -0400

    Added Right - Left Scroll and Control Network Reachability

--- Flickr_Recent/ViewController.h
+++ Flickr_Recent/ViewController.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Reachability.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIGestureRecognizerDelegate>
 
-
+@property (nonatomic) Reachability *reachability;
+@property (nonatomic) BOOL reachable;
 @end
 
--- Flickr_Recent/ViewController.m
+++ Flickr_Recent/ViewController.m
@@ -9,18 +9,23 @@
 #import "ViewController.h"
 #import "MBProgressHUD.h"
 
+
 static NSString * const API_KEY = @"d515b0f9f2c88d498153db3c68649bbe";
 
-static NSString * const recentPhotosURL = @"https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=%@&format=json&nojsoncallback=1";
+static NSString * const recentPhotosURL = @"https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&&api_key=%@&format=json&nojsoncallback=1";
+//&per_page=2,,, &page optional parmeters
 
 @interface ViewController (){
     NSArray *recentPhotos;
     NSMutableArray *urlsArray;
     int photoIndex;
     MBProgressHUD *HUD;
+   
 }
 @property (weak, nonatomic) IBOutlet UIImageView *imgView;
 @property (weak, nonatomic) IBOutlet UILabel *picTitle;
+@property (weak, nonatomic) IBOutlet UIButton *statusButton;
+
 
 @end
 
@@ -29,22 +34,48 @@ static NSString * const recentPhotosURL = @"https://api.flickr.com/services/rest
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self getRecentPhotos];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(checkForNetwork) name:kReachabilityChangedNotification object:nil];
+    self.reachability = [Reachability reachabilityForInternetConnection];
+    [self.reachability startNotifier];
     
+    [self checkForNetwork];
+    
+    UISwipeGestureRecognizer * swipeleft=[[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeLeft:)];
+    swipeleft.direction=UISwipeGestureRecognizerDirectionLeft;
+    [self.view addGestureRecognizer:swipeleft];
+    
+    UISwipeGestureRecognizer * swipeRight =[[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeRight:)];
+    swipeRight.direction=UISwipeGestureRecognizerDirectionRight;
+    [self.view addGestureRecognizer:swipeRight];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)checkForNetwork
+{
+    // check if we've got network connectivity
+    Reachability *myNetwork = [Reachability reachabilityWithHostName:@"www.google.com"];
+    NetworkStatus myStatus = [myNetwork currentReachabilityStatus];
+    
+    if(myStatus == NotReachable) {
+        self.reachable = NO;
+        [self.statusButton setImage:[UIImage imageNamed:@"nowifi"] forState:UIControlStateNormal];
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Connection error" message:@"Please check your internet connetion Settings!" delegate:self cancelButtonTitle:@"Ok!" otherButtonTitles:nil, nil];
+        [alert show];
+    }else{
+        self.reachable = YES;
+        [self.statusButton setImage:[UIImage imageNamed:@"wifi"] forState:UIControlStateNormal];
+        if (!recentPhotos) {[self getRecentPhotos];};
+    }
+  
+}
+-(void)getRecentPhotos{
     
     HUD = [[MBProgressHUD alloc]init];
     [self.view addSubview:HUD];
     HUD.mode = MBProgressHUDModeIndeterminate;
     HUD.labelText = @"Loading";
     [HUD show:YES];
-    
-    UISwipeGestureRecognizer * swipeleft=[[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeleft:)];
-    swipeleft.direction=UISwipeGestureRecognizerDirectionLeft;
-    [self.view addGestureRecognizer:swipeleft];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
--(void)getRecentPhotos{
     
     photoIndex = 0;
     
@@ -64,7 +95,7 @@ static NSString * const recentPhotosURL = @"https://api.flickr.com/services/rest
                 
                 dispatch_async(dispatch_get_main_queue(), ^{
                     [self.imgView setImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:urlsArray[photoIndex]]]];
-                    photoIndex++;
+                    
                     self.picTitle.text = recentPhotos[photoIndex][@"title"];
                     HUD.hidden = YES;
                 });
@@ -90,26 +121,40 @@ static NSString * const recentPhotosURL = @"https://api.flickr.com/services/rest
     
 }
 
--(void)swipeleft:(UIGestureRecognizer*)gesture{
+-(void)swipeLeft:(UIGestureRecognizer*)gesture{
     
-    if(photoIndex >= urlsArray.count) return;
+    if(photoIndex+1 >= urlsArray.count) return;
+    if([self reachable]){
+        photoIndex++;
+        [UIView transitionWithView:self.imgView duration:0.5
+                           options:UIViewAnimationOptionTransitionFlipFromRight animations:^{
+                               self.imgView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:urlsArray[photoIndex]]];
+                               self.picTitle.text = recentPhotos[photoIndex][@"title"];
+                           } completion:nil];
+        
+        
+    }
+}
+
+-(void)swipeRight:(UIGestureRecognizer*)gesture{
     
-    
-    [UIView transitionWithView:self.imgView duration:0.5
-                       options:UIViewAnimationOptionTransitionFlipFromRight animations:^{
-                           self.imgView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:urlsArray[photoIndex]]];
-                           self.picTitle.text = recentPhotos[photoIndex][@"title"];
-                       } completion:nil];
-    photoIndex++;
-    
-    
+    if(!photoIndex) return;
+    if([self reachable]){
+        photoIndex--;
+        [UIView transitionWithView:self.imgView duration:0.5
+                           options:UIViewAnimationOptionTransitionFlipFromLeft animations:^{
+                               self.imgView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:urlsArray[photoIndex]]];
+                               self.picTitle.text = recentPhotos[photoIndex][@"title"];
+                           } completion:nil];
+       
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 
 @end
